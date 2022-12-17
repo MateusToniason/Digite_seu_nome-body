126a60c35e5ba28f3344724b6f712e5aa2a4f3bc
Mateus <108558575+MateusToniason@users.noreply.github.com>
2022-12-16 22:11:35 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+var nome = prompt('Digite seu nome aqui:')
+document.write(`Olá, <strong>${nome}</strong>! seu nome tem ${nome.length} letras </br>`)
+document.write(`Seu nome em maiúscuulas é <span>${nome.toUpperCase()}</span> </br>`)
+document.write(`Seu nome em minúsculas é <span>${nome.toLowerCase()}</span> </br>`)
